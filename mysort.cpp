f76f5c4ca4e962e9f89bb0b8bfa7af27b3e097dd
yinda <13616215462@139.com>
2024-03-30 09:27:24 +0000

    add bubble sort

--- mysort.cpp
+++ mysort.cpp
@@ -6,16 +6,26 @@ const int arrSize = 8;
 void swap(int &a, int &b);	// 交换值
 void arr_display(int arr[]);	// 展示数组
 
-void sort_bubble(int arr[]);	// 冒泡排序
+void sort_bubble(int arr[], char mode='D');	// 冒泡排序
 
 int main(){
 	int arr[] = {5, 8, 3, 6, 7, 2, 1, 9};
 	arr_display(arr);
-	cout << "arr[2]=" << arr[2] << "; arr[3]=" << arr[3] << endl;
-	swap(arr[2], arr[3]);
-	cout << "arr[2]=" << arr[2] << "; arr[3]=" << arr[3] << endl;
+	
+	sort_bubble(arr);	
+
 	arr_display(arr);
 	return 0;
+}
+
+void sort_bubble(int arr[], char mode){
+	for (int i = 0; i < arrSize-1; i++){
+		for (int j = i; j >= 0; j--){
+			if (mode == 'D') {if (arr[j] > arr[j+1]){swap(arr[j], arr[j+1]);}}	// 升序
+			else if (mode == 'I') {if (arr[j] < arr[j+1]){swap(arr[j], arr[j+1]);}}	// 降序
+			else {cout << "mode error!" << endl;}
+		}
+	}	
 }
 
 // 利用引用传递交换数值
