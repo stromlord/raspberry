66c516fa22565c5ec5d718d68b863793f9547f3d
yinda <13616215462@139.com>
2024-04-01 00:14:06 +0100

    h_cpp

--- demo.cpp
+++ demo.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
+#include "coordin.h"
 using namespace std;
 
-const int ArSize = 8;
-int sum_arr(int arr[], int n);
-
 int main() {
-	int cookies[ArSize] = { 1,2,4,8,16,32,64,128 };
-	cout << cookies << " = array address, ";
-	cout << sizeof cookies << " = sizeof cookies\n";
-	int sum = sum_arr(cookies, ArSize);
-	cout << "Total cookies eaten: " << sum << endl;
-	sum = sum_arr(cookies, 3);
-	cout << "First three eaters ate " << sum << " cookies.\n";
-	sum = sum_arr(cookies + 4, 4);
-	cout << "Last four eaters ate " << sum << " cookies.\n";
-	return 0;
-}
+	rect rplace;
+	polar pplace;
 
-int sum_arr(int arr[], int n) {
-	int total = 0;
-	cout << arr << " = arr, ";
-	cout << sizeof arr << " = sizeof arr\n";
-	for (int i = 0; i < n; i++)
-		total = total + arr[i];
-	return total;
+	cout << "Enter the x and y values:";
+	while (cin >> rplace.x >> rplace.y) {
+		pplace = rect_to_polar(rplace);
+		show_polar(pplace);
+		cout << "Next two number (q to quit): ";
+	}
+	cout << "Bye!\n";
+	return 0;
 }
