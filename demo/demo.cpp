195a4a4293fdaff0a1ee4840a1638c3bd50953a4
yinda <13616215462@139.com>
2024-04-03 16:11:27 +0100

    class demo

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,16 +1,49 @@
-#ifndef DEMO_H_
-#define DEMO_H_
+#include <iostream>
+#include "demo.h"
 
-#include <string>
+void demo::acquire(const std::string & co, long n, double pr){
+  company = co;
+  if (n < 0){
+    std::cout << "Number of shares can't be negative;" << company << " shares set to 0.\n";
+    shares = 0;
+  }
+  else
+    shares = n;
+  share_val = pr;
+  set_tot(); 
+}
 
-class Stock{
-  private:
-    std::string company;
-    long shates;
-    double share_val;
-    double total_val;
-    void set_yoy(){total_val = shares * share_val;}
-  public:
-    void acquire(const std::string & co, long n, double pr);
-    void 
+void demo::buy(long num, double price){
+  if (num < 0)
+    std::cout << "Number of shares purchased can't be negative.\n";
+  else{
+    shares += num;
+    share_val = price;
+    set_tot();
+  }
+}
+
+void demo::sell(long num, double price){
+  using std::cout;
+  if (num < 0)
+    cout << "Number of shares purchased can't be negative.\n";
+  else if (num > shares)
+    cout << "You can't sell more than you have!\n";
+  else{
+    shares -= num;
+    share_val = price;
+    set_tot();
+  }
+}
+
+void demo::update(double price){
+  share_val = price;
+  set_tot();
+}
+
+void demo::show(){
+  std::cout << "Company: " << company 
+            << " Shares: " << shares << "\n"
+            << " Share Price: $" << share_val
+            << " Total Worth: $" << total_val << "\n";
 }
