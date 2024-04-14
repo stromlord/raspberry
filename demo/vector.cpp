4578e796945ab7e15d71010356e947ddb91c9594
yinda <13616215462@139.com>
2024-04-14 16:56:02 +0100

    vector

---
+++ demo/vector.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include "vector.h"
+using std::sqrt;
+using std::sin;
+using std::cos;
+using std::atan;
+using std::atan2;
+using std::cout;
+
+namespace VECTOR{
+    const double Rad_to_deg = 45.0 / atan(1.0);
+     
+}
