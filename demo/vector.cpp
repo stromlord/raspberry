b8547917274b9017d69e2ccd0e09caddaac81ac3
yinda <13616215462@139.com>
2024-04-15 06:05:26 +0100

    vimrc

--- demo/vector.cpp
+++ demo/vector.cpp
@@ -8,6 +8,21 @@ using std::atan2;
 using std::cout;
 
 namespace VECTOR{
-    const double Rad_to_deg = 45.0 / atan(1.0);
-     
+    const double Rad_to_deg = 45.0 / atan(1.0);		// 度数转换 
+    // private方法实现
+	void Vector::set_mag(){mag=sqrt(x*x+y*y);}
+	void Vector::set_ang(){
+		if (x==0.0&&y==0.0)
+			ang = 0.0;
+		else
+			ang = atan2(y, x);
+	}
+	void Vector::set_x(){x = mag * cos(ang);}
+	void Vector::set_y(){y = mag * sin(ang);}
+	
+	// public方法实现
+	Vector::Vector(){
+		x = y = mag = ang = 0.0;
+		mode = RECT;
+	}
 }
